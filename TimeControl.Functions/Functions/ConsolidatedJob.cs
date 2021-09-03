cbd1aad4b96bbee2ef2fe97b5ed75c512db5efec
Andrés Restrepo <jandru0512@gmail.com>
2021-09-02 20:31:13 -0500

    ConsolidatedRefactor

--- TimeControl.Functions/Functions/ConsolidatedJob.cs
+++ TimeControl.Functions/Functions/ConsolidatedJob.cs
@@ -21,9 +21,7 @@ namespace TimeControl.Functions
             log.LogInformation($"Starting job to consolidate information at: {DateTime.Now}");
 
             // Records not consolidated yet
-            string recordFilter = TableQuery.GenerateFilterConditionForBool(nameof(RecordEntity.Consolidated), QueryComparisons.Equal, false);
-            TableQuery<RecordEntity> recordQuery = new TableQuery<RecordEntity>().Where(recordFilter);
-            TableQuerySegment<RecordEntity> records = await recordTable.ExecuteQuerySegmentedAsync(recordQuery, null);
+            List<RecordEntity> records = await NotConsolidated(recordTable);
 
             // Created registers
             int created = 0;
@@ -56,66 +54,25 @@ namespace TimeControl.Functions
                     }
 
                     // Consolidated by employee and date
-                    string consolidatedDateFilter = TableQuery.GenerateFilterConditionForDate(nameof(ConsolidatedEntity.Date), QueryComparisons.GreaterThanOrEqual, recordDate);
-                    string consolidatedEmployeeFilter = TableQuery.GenerateFilterConditionForInt(nameof(ConsolidatedEntity.EmployeeId), QueryComparisons.Equal, employee);
-
-                    TableQuery<ConsolidatedEntity> consolidatedQuery = new TableQuery<ConsolidatedEntity>().Where(TableQuery.CombineFilters(consolidatedEmployeeFilter, TableOperators.And, consolidatedDateFilter));
-                    TableQuerySegment<ConsolidatedEntity> consolidateds = await consolidatedTable.ExecuteQuerySegmentedAsync(consolidatedQuery, null);
-                    ConsolidatedEntity consolidated = consolidateds.FirstOrDefault();
-
+                    ConsolidatedEntity consolidated = await Consolidated(consolidatedTable, employee, recordDate);
                     List<RecordEntity> timeRecords = records.Where((x) => x.EmployeeId == employee && x.CreatedAt.Date == recordDate).ToList();
 
                     // reduce records getting minutes
-                    int minutes = timeRecords.Aggregate(new string[] { "", "0" }, (acum, row) =>
-                    {
-                        if (row.Type == (int)RecordTypes.In)
-                        {
-                            return new string[] { row.CreatedAt.ToString(), acum[1] };
-                        }
-
-                        DateTime date = DateTime.Parse(acum[0]);
-                        int counter = int.Parse(acum[1]);
-
-                        TimeSpan time = row.CreatedAt - date;
-                        counter += (int)time.TotalMinutes;
-
-                        return new string[] { "", counter.ToString() };
-                    },
-                    (acum) => int.Parse(acum[1]));
+                    int minutes = timeRecords.Aggregate(new string[] { "", "0" }, (acum, row) => { return GetMinutes(acum, row); }, (acum) => int.Parse(acum[1]));
 
                     // Write into consolidated table
                     if (consolidated == null)
                     {
-                        TableOperation create = TableOperation.Insert(new ConsolidatedEntity
-                        {
-                            Date = recordDate,
-                            ETag = "*",
-                            EmployeeId = employee,
-                            Minutes = minutes,
-                            PartitionKey = "CONSOLIDATED",
-                            RowKey = Guid.NewGuid().ToString()
-                        });
-
-                        await consolidatedTable.ExecuteAsync(create);
+                        await CreateConsolidated(consolidatedTable, created, employee, recordDate, minutes);
                         created++;
                     }
                     else
                     {
-                        consolidated.Minutes += minutes;
-                        TableOperation update = TableOperation.Replace(consolidated);
-
-                        await consolidatedTable.ExecuteAsync(update);
+                        await UpdateConsolidated(consolidatedTable, consolidated, minutes);
                         updated++;
                     }
 
-                    foreach (RecordEntity timeRecord in timeRecords)
-                    {
-                        // Update consolidated propperty
-                        timeRecord.Consolidated = true;
-
-                        TableOperation update = TableOperation.Replace(timeRecord);
-                        await recordTable.ExecuteAsync(update);
-                    }
+                    await UpdateRecords(recordTable, timeRecords);
 
                     // Add to processed list
                     result.Add(recordDate.ToString());
@@ -123,6 +80,77 @@ namespace TimeControl.Functions
             }
 
             log.LogInformation($"{created} new records have been consolidated and {updated} have been updated.");
+        }
+
+        private static async Task UpdateRecords(CloudTable recordTable, List<RecordEntity> timeRecords)
+        {
+            foreach (RecordEntity timeRecord in timeRecords)
+            {
+                // Update consolidated propperty
+                timeRecord.Consolidated = true;
+
+                TableOperation update = TableOperation.Replace(timeRecord);
+                await recordTable.ExecuteAsync(update);
+            }
+        }
+
+        private static async Task UpdateConsolidated(CloudTable consolidatedTable, ConsolidatedEntity consolidated, int minutes)
+        {
+            consolidated.Minutes += minutes;
+            TableOperation update = TableOperation.Replace(consolidated);
+
+            await consolidatedTable.ExecuteAsync(update);
+        }
+
+        private static async Task<int> CreateConsolidated(CloudTable consolidatedTable, int created, int employee, DateTime recordDate, int minutes)
+        {
+            TableOperation create = TableOperation.Insert(new ConsolidatedEntity
+            {
+                Date = recordDate,
+                ETag = "*",
+                EmployeeId = employee,
+                Minutes = minutes,
+                PartitionKey = "CONSOLIDATED",
+                RowKey = Guid.NewGuid().ToString()
+            });
+
+            await consolidatedTable.ExecuteAsync(create);
+            created++;
+            return created;
+        }
+
+        private static string[] GetMinutes(string[] acum, RecordEntity row)
+        {
+            if (row.Type == (int)RecordTypes.In)
+            {
+                return new string[] { row.CreatedAt.ToString(), acum[1] };
+            }
+
+            DateTime date = DateTime.Parse(acum[0]);
+            int counter = int.Parse(acum[1]);
+
+            TimeSpan time = row.CreatedAt - date;
+            counter += (int)time.TotalMinutes;
+
+            return new string[] { "", counter.ToString() };
+        }
+
+        private static async Task<ConsolidatedEntity> Consolidated(CloudTable consolidatedTable, int employee, DateTime recordDate)
+        {
+            string consolidatedDateFilter = TableQuery.GenerateFilterConditionForDate(nameof(ConsolidatedEntity.Date), QueryComparisons.GreaterThanOrEqual, recordDate);
+            string consolidatedEmployeeFilter = TableQuery.GenerateFilterConditionForInt(nameof(ConsolidatedEntity.EmployeeId), QueryComparisons.Equal, employee);
+
+            TableQuery<ConsolidatedEntity> consolidatedQuery = new TableQuery<ConsolidatedEntity>().Where(TableQuery.CombineFilters(consolidatedEmployeeFilter, TableOperators.And, consolidatedDateFilter));
+            TableQuerySegment<ConsolidatedEntity> consolidateds = await consolidatedTable.ExecuteQuerySegmentedAsync(consolidatedQuery, null);
+            return consolidateds.FirstOrDefault();
+        }
+
+        private static async Task<List<RecordEntity>> NotConsolidated(CloudTable recordTable)
+        {
+            string recordFilter = TableQuery.GenerateFilterConditionForBool(nameof(RecordEntity.Consolidated), QueryComparisons.Equal, false);
+            TableQuery<RecordEntity> recordQuery = new TableQuery<RecordEntity>().Where(recordFilter);
+            TableQuerySegment<RecordEntity> records = await recordTable.ExecuteQuerySegmentedAsync(recordQuery, null);
+            return records.OrderBy((x) => x.CreatedAt).ToList();
         }
     }
 }
